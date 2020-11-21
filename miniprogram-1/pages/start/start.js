6da5d3c1934e8982ae0b07a568a0ba1e3aef1d4d
ssionii <ssionii353@gmail.com>
2020-11-22 06:55:38 +0900

    [start] 시작하기 버튼 클릭시 로그인 화면 이동

--- miniprogram-1/pages/start/start.js
+++ miniprogram-1/pages/start/start.js
@@ -3,6 +3,31 @@ const app = getApp()
 Page({
   onLoad: function () {
     console.log('page load')
-    // 통신 필요 (사용자 이름)
+    
+  },
+  logIn : function(){
+    if(app.globalData.userInfo != null){
+      wx.reLaunch({
+        url: '../main/main',
+      })
+    } else {
+      wx.showModal({
+        title: 'Apply',
+        content: 'Can we user your name and Id?',
+        success (res) {
+          if (res.confirm) {
+            console.log('"OK" is tapped')
+            // Todo : 로그인 통신
+            wx.reLaunch({
+              url: '../main/main',
+            })
+          } else if (res.cancel) {
+            console.log('"Cancel" is tapped')
+            // Todo: 앱 종료
+          }
+        }
+      })
+      
+    }
   }
 })
